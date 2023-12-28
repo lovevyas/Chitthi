0e1aab762e20c6aacc2aa0a44ac1c0a440e7dfb8
lovevyas <love1252.be21@chitkarauniversity.edu.in>
2023-12-28 15:14:44 +0530

    Begin
    Begin

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Lets Build </h1>
+
+      {/* Header */}
+
+      {/* App body */}
+        {/* Sidebar */}
+        {/* Freed */}
+        {/* Widgets */}
+    </div>
+  );
+}
+
+export default App;
