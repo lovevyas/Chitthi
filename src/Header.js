5b831332492dc557e8cde1358eb47c4cf4a1513a
Sujit <sujitkar1195@gmail.com>
2024-01-02 15:18:08 +0530

    header section completed

---
+++ src/Header.js
@@ -0,0 +1,93 @@
+import React from "react";
+import "./Header.css";
+import logo from "./Asset/logoss.png";
+import SearchIcon from "@mui/icons-material/Search";
+import HomeIcon from "@mui/icons-material/Home";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import StorefrontIcon from "@mui/icons-material/Storefront";
+import FlagIcon from "@mui/icons-material/Flag";
+import SupervisedUserCircleIcon from "@mui/icons-material/SupervisedUserCircle";
+import { Avatar } from "@mui/material";
+import { IconButton } from "@mui/material";
+import { Add, Expand } from "@mui/icons-material";
+import { Forum } from "@mui/icons-material";
+import { NotificationsActive } from "@mui/icons-material";
+import { ExpandMore } from "@mui/icons-material";
+function Header() {
+  return (
+    <div className="header">
+      {/* header left start*/}
+      <div className="header__left">
+        {/* logo image start*/}
+        <img src={logo} alt="__logo" />
+        {/* logo image end*/}
+
+        {/* header input start */}
+        <div className="header__input">
+          <SearchIcon />
+          <input placeholder="Search Chitthi" type="text" />
+        </div>
+        {/* header input end */}
+      </div>
+      {/* header left end*/}
+
+      {/* header middle start */}
+      <div className="header__center">
+        {/* header option start */}
+        <div className="header__option header__option--active">
+          <HomeIcon fontSize="large" />
+        </div>
+        {/* header option end */}
+
+        {/* header option start */}
+        <div className="header__option">
+          <FlagIcon fontSize="large" />
+        </div>
+        {/* header option end */}
+
+        {/* header option start */}
+        <div className="header__option">
+          <SubscriptionsIcon fontSize="large" />
+        </div>
+        {/* header option end */}
+
+        {/* header option start */}
+        <div className="header__option">
+          <StorefrontIcon fontSize="large" />
+        </div>
+        {/* header option end */}
+        {/* header option start */}
+        <div className="header__option">
+          <SupervisedUserCircleIcon fontSize="large" />
+        </div>
+        {/* header option end */}
+      </div>
+      {/* header middle end */}
+
+      {/* header right start */}
+      <div className="header__right">
+        {/* header info start */}
+        <div className="header__info">
+          <Avatar />
+          <h4>Sujit Kar</h4>
+          <IconButton>
+            <Add />
+          </IconButton>
+          <IconButton>
+            <Forum />
+          </IconButton>
+          <IconButton>
+            <NotificationsActive />
+          </IconButton>
+          <IconButton>
+            <ExpandMore />
+          </IconButton>
+        </div>
+        {/* header info end */}
+      </div>
+      {/* header right end */}
+    </div>
+  );
+}
+
+export default Header;
